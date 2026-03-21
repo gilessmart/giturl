6d44c4dbf5c80c383e1c363e2c919396ee033a3a
gilessmart <3227792+gilessmart@users.noreply.github.com>
2026-03-21 18:33:21 +0000

    Reorder methods in the cli module

--- src/giturl/cli.py
+++ src/giturl/cli.py
@@ -10,44 +10,6 @@ import giturl.git as git
 from giturl.url_templates import parse_template
 
 
-def parse_args() -> tuple[str, int | None, bool]:
-    parser = argparse.ArgumentParser(description="Generate a GitHub URL for a file or directory in a git repository.")
-    parser.add_argument("-l", "--line", dest="line_number", type=int, help="Line number to include in the URL", metavar="line_number")
-    parser.add_argument("-b", "--branch", dest="branch_mode", action="store_true", help="Use branch name instead of commit SHA in the URL")
-    parser.add_argument("path", type=pathlib.Path, help="Path to a file or directory in the git repository")
-    args = parser.parse_args()
-    
-    if not os.path.isfile(args.path) and not os.path.isdir(args.path):
-        parser.error(f"'{args.path}' is not an existing file or directory")
-
-    if args.line_number is not None and os.path.isdir(args.path):
-        parser.error("line number is invalid when path is a directory")
-    
-    return args.path, args.line_number, args.branch_mode
-
-
-def fail(msg: str) -> NoReturn:
-    print("Error: " + msg, file=sys.stderr)
-    sys.exit(1)
-
-
-url_configs = {
-    r"github.com[:/](?P<account>.+?)/(?P<repo>.+?).git": "https://github.com/{{account}}/{{repo}}/blob/{{ref}}{{path}}{#L{line_number}}",
-    r"bitbucket.org[:/](?P<account>.+?)/(?P<repo>.+?).git": "https://bitbucket.org/{{account}}/{{repo}}/src/{{ref}}{{path}}{#line-{line_number}}",
-}
-
-
-def generate_url(remote_url: str, url_args: dict[str, str | None]) -> str:
-    for pattern, template_str in url_configs.items():
-        match = re.search(pattern, remote_url)
-        if match:
-            template = parse_template(template_str)
-            quoted_args = { k: quote(v) for (k, v) in (match.groupdict() | url_args).items() if v is not None }
-            return template.apply(quoted_args)
-    
-    fail(f"No config matched remote URL {remote_url}")
-
-
 def main():
     path_arg, line_number, branch_mode = parse_args()
 
@@ -77,6 +39,27 @@ def main():
     })
 
     print(url)
+
+
+def parse_args() -> tuple[str, int | None, bool]:
+    parser = argparse.ArgumentParser(description="Generate a GitHub URL for a file or directory in a git repository.")
+    parser.add_argument("-l", "--line", dest="line_number", type=int, help="Line number to include in the URL", metavar="line_number")
+    parser.add_argument("-b", "--branch", dest="branch_mode", action="store_true", help="Use branch name instead of commit SHA in the URL")
+    parser.add_argument("path", type=pathlib.Path, help="Path to a file or directory in the git repository")
+    args = parser.parse_args()
+    
+    if not os.path.isfile(args.path) and not os.path.isdir(args.path):
+        parser.error(f"'{args.path}' is not an existing file or directory")
+
+    if args.line_number is not None and os.path.isdir(args.path):
+        parser.error("line number is invalid when path is a directory")
+    
+    return args.path, args.line_number, args.branch_mode
+
+
+def fail(msg: str) -> NoReturn:
+    print("Error: " + msg, file=sys.stderr)
+    sys.exit(1)
 
 
 def get_ref(repo_root: str, branch_mode: bool, local_branch: str | None):
@@ -113,3 +96,19 @@ def get_repo_path(repo_root, full_path):
     if os.path.samefile(full_path, repo_root):
         return ""
     return "/" + os.path.relpath(full_path, repo_root).replace(os.sep, "/")
+
+
+def generate_url(remote_url: str, url_args: dict[str, str | None]) -> str:
+    url_configs = {
+        r"github.com[:/](?P<account>.+?)/(?P<repo>.+?).git": "https://github.com/{{account}}/{{repo}}/blob/{{ref}}{{path}}{#L{line_number}}",
+        r"bitbucket.org[:/](?P<account>.+?)/(?P<repo>.+?).git": "https://bitbucket.org/{{account}}/{{repo}}/src/{{ref}}{{path}}{#line-{line_number}}",
+    }
+
+    for pattern, template_str in url_configs.items():
+        match = re.search(pattern, remote_url)
+        if match:
+            template = parse_template(template_str)
+            quoted_args = { k: quote(v) for (k, v) in (match.groupdict() | url_args).items() if v is not None }
+            return template.apply(quoted_args)
+    
+    fail(f"No config matched remote URL {remote_url}")
