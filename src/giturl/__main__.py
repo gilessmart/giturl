661d0f50c43b991e34c6542596bd5503c0916138
gilessmart <3227792+gilessmart@users.noreply.github.com>
2026-07-09 16:30:40 +0100

    Add a debugger config and clarify why __main__.py exists

--- src/giturl/__main__.py
+++ src/giturl/__main__.py
@@ -1,4 +1,9 @@
 from .cli import main
 
+# This is the execution path used when the app is invoked with `python -m giturl`,
+# which is how the VSCode debugger invokes it.
+# It would be possible for the debugger to run the script ([project.scripts]),
+# and not have this file, except that wouldn't work in Windows because on Windows pip
+# generates an .exe file in the venv instead of a plain script.
 if __name__ == "__main__":
     main()
